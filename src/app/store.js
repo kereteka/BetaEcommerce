07dae5ce69319cb405758521023c6380243bd51e
kereteka <mehmet.sengun1@gmail.com>
2023-11-28 02:21:49 +0300

    ff

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../features/product/api";
 import { productReducer } from "../features/product/productSlice";
 import { searchTermReducer } from "../features/product/searchTermSlice";
-import { cartReducer } from "../Features/cart/cartSlice";
+import { cartReducer } from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
